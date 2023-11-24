a811ceaf3baa611c33dcb89c1b9b7ac687eba037
CodeBlack04 <tauhidur.rahman1517@gmail.com>
2023-11-24 17:31:04 +0600

    Added Animations to buttons

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('policy/', views.policy, name='policy'),
